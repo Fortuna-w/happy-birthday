dfdceb3596d1bb735f3bd518b9ded2fe59d40030
Fortuna-w <96736721+Fortuna-w@users.noreply.github.com>
2024-11-17 18:04:45 +0000

    Update main.js

--- script/main.js
+++ script/main.js
@@ -317,11 +317,11 @@ const animationTimeline = () => {
       0.3
     )
     // 插入蛋糕动画：星星动画结束后显示蛋糕
-    .to(".cake-container", 1, {
+    .staggerFrom(".cake-container", 1, {
       opacity: 1,
-      y: 0,
+      y: 50,
       ease: "bounce.out"
-    }, "+=1.5") // 延迟0.5秒，确保星星动画结束后蛋糕出现
+    }, 0.2， "+=3") // 延迟0.5秒，确保星星动画结束后蛋糕出现
     .to(".six", 0.5, {
       opacity: 0,
       y: 30,
